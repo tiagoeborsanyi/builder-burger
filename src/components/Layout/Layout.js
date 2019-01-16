78afbf2d139afb3154f32c44511c646ec64c5d80
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-01-16 20:56:20 -0200

    add sctruture burger ingredients

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
             {props.children}
-        <main>
-
         </main>
     </Aux>
 );

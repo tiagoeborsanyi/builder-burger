134e87de5c958ecaa4a63134d0084507b1e80dc5
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-02-06 10:47:41 -0200

    add react-router-dom

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,7 +93,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert('You continue!!');
-        this.setState({loading: true});
+        /* this.setState({loading: true});
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -114,7 +114,8 @@ class BurgerBuilder extends Component {
             })
             .catch(error => {
                 this.setState({loading: false, purchasing: false});
-            });
+            }); */
+        this.props.history.push('/checkout');
     }
 
     render() {

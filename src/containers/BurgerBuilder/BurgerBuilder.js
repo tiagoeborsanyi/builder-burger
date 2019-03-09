8c3df9559c4a3707dfb8c3788e5b329ec0ff98be
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-03-08 22:29:05 -0300

    finish the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,14 +31,14 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
-        axios.get('/ingredients.json')
-            .then(response => {
-                //console.log(response);
-                this.setState({ingredients: response.data});
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
+        // axios.get('/ingredients.json')
+        //     .then(response => {
+        //         //console.log(response);
+        //         this.setState({ingredients: response.data});
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true});
+        //     });
     }
 
     updatePurchaseState (ingredients) {
